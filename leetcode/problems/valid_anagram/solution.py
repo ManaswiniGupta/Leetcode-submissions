c465ec6a99aa505e7d235dd56f066ec2790a4291
Manaswini Gupta <144774277+ManaswiniGupta@users.noreply.github.com>
2025-01-10 20:37:06 +0000

    Sync LeetCode submission - Valid Anagram (python)

---
+++ leetcode/problems/valid_anagram/solution.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s)!=len(t):
+            return False
+        for i in s:
+            if i in t:
+                t=t.replace(i,"",1)
+            else:
+                return False
+        return True
