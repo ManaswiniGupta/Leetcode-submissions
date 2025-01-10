d0faeefb74f9030b235046179a7029a872cf8534
Manaswini Gupta <144774277+ManaswiniGupta@users.noreply.github.com>
2025-01-10 20:20:29 +0000

    Sync LeetCode submission - Pow(x, n) (python)

---
+++ leetcode/problems/pow(x,_n)/solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        if n==0:
+            return 1
+        elif n<0:
+            return 1/self.myPow(x,-n)
+        elif n % 2 == 0:
+            half = self.myPow(x, n // 2)
+            return half * half
+        else:
+            return x*self.myPow(x,n-1)
