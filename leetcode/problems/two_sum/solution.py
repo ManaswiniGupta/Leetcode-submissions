4351ffca66e097abae481f3219c7ab24f690a645
Manaswini Gupta <144774277+ManaswiniGupta@users.noreply.github.com>
2025-03-24 20:34:49 +0000

    Sync LeetCode submission - Two Sum (python3)

--- leetcode/problems/two_sum/solution.py
+++ leetcode/problems/two_sum/solution.py
@@ -1,15 +1,19 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        # for i in range(len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         if nums[i]+nums[j]==target:
+        #             return [i,j]
+        #         else:
+        #             continue
+        
+        diction={}
         n=len(nums)
         for i in range(n):
-            for j in range(i+1,n):
-                if target==nums[i]+nums[j]:
-                    return [i,j]
-                else:
-                    pass
-        
+            diction[nums[i]]=i
+        for i in range(n):
+            complement=target-nums[i]
+            if complement in diction and diction[complement] != i:
+                return [i, diction[complement]]
+        return [] 
