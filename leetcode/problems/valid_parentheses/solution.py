d02e1401f117603ee33c4c354c7138a8c1f4a02a
Manaswini Gupta <144774277+ManaswiniGupta@users.noreply.github.com>
2026-06-03 18:06:51 +0000

    Sync LeetCode submission - Valid Parentheses (python3)

---
+++ leetcode/problems/valid_parentheses/solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s)<2 or s[0] in [')','}',']']:
+            return False
+        stack=[]
+        for i in s:
+            if i in ['{','[','(']:
+                stack.append(i)
+            else:
+                if not stack:
+                    return False
+                elif i==']' and stack[-1]=='[':
+                    stack.pop()
+                elif i==')' and stack[-1]=='(':
+                    stack.pop()
+                elif i=='}' and stack[-1]=='{':
+                    stack.pop()
+                else:
+                    return False
+        return len(stack)==0
+    
+    
+
+        
