d43f7613c7ecb38927819b2739ac22de4c304b2f
Manaswini Gupta <144774277+ManaswiniGupta@users.noreply.github.com>
2026-06-03 17:20:47 +0000

    Sync LeetCode submission - Majority Element (python3)

--- leetcode/problems/majority_element/solution.py
+++ leetcode/problems/majority_element/solution.py
@@ -1,15 +1,22 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count=0
-        candidate=0
-        for nums in nums:
-            if count==0:
-                candidate=nums
-            if nums==candidate:
-                count+=1
-            else:
-                count-=1
-        return candidate
+        # count=0
+        # candidate=0
+        # for nums in nums:
+        #     if count==0:
+        #         candidate=nums
+        #     if nums==candidate:
+        #         count+=1
+        #     else:
+        #         count-=1
+        # return candidate
+        threshold=len(nums)//2
+        d={}
+        for i in nums:
+            d[i]=d.get(i,0)+1
+            if d[i]>threshold:
+                return i
+
         
         
 
