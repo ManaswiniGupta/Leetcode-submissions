b74f092f29da2132065cffeea905657e9055f4b0
Manaswini Gupta <144774277+ManaswiniGupta@users.noreply.github.com>
2026-06-10 12:07:49 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.3 MB (54.96%)

---
+++ leetcode/1927-maximum-ascending-subarray-sum/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxAscendingSum(self, nums: List[int]) -> int:
+        total=nums[0]
+        ans=nums[0]
+        for i in range(1,len(nums)):
+            if nums[i-1]<nums[i]:
+                total+=nums[i]
+                
+            else:
+                ans=max(ans, total)
+                total=nums[i]
+        ans=max(ans,total)
+        return ans
+
+        
+        
