cd67607c49e1e1233fab1b1337452b2c96b6c285
Manaswini Gupta <144774277+ManaswiniGupta@users.noreply.github.com>
2026-06-17 10:52:25 +0000

    Sync LeetCode submission Runtime - 3 ms (73.81%), Memory - 19.3 MB (73.53%)

---
+++ leetcode/2308-divide-array-into-equal-pairs/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def divideArray(self, nums: List[int]) -> bool:
+        d=Counter(nums)
+        if len(nums)%2!=0:
+            return False
+        for i in d.values():
+            if i%2!=0:
+                return False
+        return True
+        
